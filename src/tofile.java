69b152c3876001b4b2b231081aaa075fb99c171c
Jack Eller <admin@kilobear.com>
2015-02-07 20:28:37 -0500

    Started toFile

---
+++ src/tofile.java
@@ -0,0 +1,26 @@
+//written by Jack Eller
+
+import java.io.*;
+
+public class tofile{
+
+    public void to(user c){
+
+	try
+	    {
+         FileOutputStream fileOut = new FileOutputStream("/people/" + c.getID() + ".name");
+         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+         out.writeObject(c);
+         out.close();
+         fileOut.close();
+         System.out.println("Saved to " + "/people/" + c.getID() + ".name");
+	    }catch(IOException i)
+	    {
+		i.printStackTrace();
+	    }
+
+    }
+
+
+
+}
